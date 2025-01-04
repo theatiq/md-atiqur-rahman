e680260cd9cb4741efe6bfeb9acddd20d630c86a
theatiq <theatiq@gmail.com>
2025-01-04 10:49:19 +0600

    experience and skills images added

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import htmlImg from "/src/assets/html.png";
+import cssImg from "/src/assets/css.png";
+import jsImg from "/src/assets/js.png";
+import mongodbImg from "/src/assets/mongodb.png";
+import firebaseImg from "/src/assets/firebase.png";
+import nodeImg from "/src/assets/node.png";
+import expressImg from "/src/assets/express.png";
+import gitImg from "/src/assets/git.png";
+import githubImg from "/src/assets/github.png";
+import reactImg from "/src/assets/react.png";
+import pythonImg from "/src/assets/python.png";
+import reactRouterImg from "/src/assets/router.png";
 
 const Skills = () => {
   return (
@@ -19,20 +30,39 @@ const Skills = () => {
             <img src={htmlImg} alt="HTML5" className="rounded-xl" />
           </figure>
           <figure className="px-5 pt-5">
-            <img src={htmlImg} alt="HTML5" className="rounded-xl" />
+            <img src={cssImg} alt="CSS" className="rounded-xl" />
           </figure>
           <figure className="px-5 pt-5">
-            <img src={htmlImg} alt="HTML5" className="rounded-xl" />
+            <img src={jsImg} alt="JS" className="rounded-xl" />
           </figure>
           <figure className="px-5 pt-5">
-            <img src={htmlImg} alt="HTML5" className="rounded-xl" />
+            <img src={mongodbImg} alt="Mongodb" className="rounded-xl" />
           </figure>
           <figure className="px-5 pt-5">
-            <img src={htmlImg} alt="HTML5" className="rounded-xl" />
+            <img src={firebaseImg} alt="firebase" className="rounded-xl" />
           </figure>
           <figure className="px-5 pt-5">
-            <img src={htmlImg} alt="HTML5" className="rounded-xl" />
+            <img src={nodeImg} alt="node" className="rounded-xl" />
           </figure>
+          <figure className="px-5 pt-5">
+            <img src={expressImg} alt="express" className="rounded-xl" />
+          </figure>
+          <figure className="px-5 pt-5">
+            <img src={gitImg} alt="git" className="rounded-xl" />
+          </figure>
+          <figure className="px-5 pt-5">
+            <img src={githubImg} alt="github" className="rounded-xl" />
+          </figure>
+          <figure className="px-5 pt-5">
+            <img src={reactImg} alt="react" className="rounded-xl" />
+          </figure>
+          <figure className="px-5 pt-5">
+            <img src={reactRouterImg} alt="router" className="rounded-xl" />
+          </figure>
+          <figure className="px-5 pt-5">
+            <img src={pythonImg} alt="python" className="rounded-xl" />
+          </figure>
+
           {/* <div className="card-body items-center text-center">
             <h2 className="card-title">Shoes!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
